45c91b079611aa407fad8644891ef9a848809582
indhud2005 <vtu25768@veltech.edu.in>
2026-01-01 18:29:38 +0530

    first

---
+++ inserts.sql
@@ -0,0 +1,12 @@
+INSERT INTO Student VALUES("1MS23CS001", "Arjun Kumar", "arjun1@gmail.com", 9845022334, "IT");
+INSERT INTO Student VALUES("1MS23CS002", "Sneha ", "sneha2@gmail.com", 9845011223, "CSE");
+INSERT INTO Student VALUES("1MS23CS003", "Rahul", "rahul3@gmail.com", 9845033445, "CSE");
+INSERT INTO Student VALUES("1MS23CS004", "Priya", "priya4@gmail.com", 9845044556, "ECE");
+INSERT INTO Student VALUES("1MS23CS005", "Amit", "amit5@gmail.com", 9845055667, "IT");
+
+
+INSERT INTO Course VALUES("CS101", "Database Management", 101, "1MS23CS001", "prof_smith@vtu.edu");
+INSERT INTO Course VALUES("IS102", "Data Structures", 102, "1MS23CS002", "prof_jones@vtu.edu");
+INSERT INTO Course VALUES("CS103", "Operating Systems",101, "1MS23CS003", "prof_smith@vtu.edu");
+INSERT INTO Course VALUES("EC301", "Digital Electronics", 103, "1MS23CS004", "prof_kumar@vtu.edu");
+INSERT INTO Course VALUES("IS204", "Web Programming" , 104, "1MS23CS004", "prof_leila@vtu.edu");
